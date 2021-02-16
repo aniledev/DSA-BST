fe7df1d7005a8ed29a510a36a76b4e7ee58b2550
aniledev <elinamcgill@gmail.com>
2021-02-16 13:51:46 -0500

    FEAT: write base case for createBalancedBST function

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -10,6 +10,12 @@ class BinarySearchTree {
 
 // create a balanced binary search tree
 function createBalancedBST(arr) {
+  // write base case
+  if (arr.length === 1) {
+    const node = new BinarySearchTree(arr[0]);
+    return nod;
+  }
+
   const middleIndex = Math.floor(arr.length / 2);
   const middleValue = arr[middleIndex];
 
@@ -20,6 +26,11 @@ function createBalancedBST(arr) {
 
   const leftSubTree = createBalancedBST(left);
   const rightSubTree = createBalancedBST(right);
+
+  const node = new BinarySearchTree(middleValue);
+  node.left = leftSubTree;
+  node.right = rightSubTree;
+  return node;
 }
 
-console.dir(createBalancedBST([1, 2, 3, 5, 7, 9, 11]), [(depth: null)]);
+console.dir(createBalancedBST([1, 2, 3, 5, 7, 9, 11]), { depth: null });
